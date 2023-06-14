0e8811ca490d8b785cd5e6b7032e6b698ddddb3e
fabiolorenzi <fabiolorenzi13975@gmail.com>
2023-06-14 22:32:55 +0100

    updated urls file and users views

--- food_organizer_backend/views/users.py
+++ food_organizer_backend/views/users.py
@@ -6,8 +6,15 @@ from ..serializers.user_serializer import UserSerializer
 from ..models.user import User
 
 
-@api_view(["GET"])
+@api_view(["GET", "POST"])
 def users_list(request):
-    all_users = User.object.all()
-    serializer = UserSerializer(all_users, many=True)
-    return JsonResponse(serializer.data, safe=False)
+    if request.method == "GET":
+        all_users = User.objects.all()
+        serializer = UserSerializer(all_users, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    elif request.method == "POST":
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
