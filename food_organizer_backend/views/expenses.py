f2a79220adecaa0c32d5b49aaccef1cd854affbb
fabiolorenzi <fabiolorenzi13975@gmail.com>
2023-06-16 16:29:34 +0100

    added logic to get query parameters to set timings in expenses

--- food_organizer_backend/views/expenses.py
+++ food_organizer_backend/views/expenses.py
@@ -20,6 +20,14 @@ def expense_list(request):
     if id:
         if request.method == "GET":
             all_plans = Expense.objects.filter(user_id=user_id)
+
+            min_date = request.GET.get("min_date", None)
+            if min_date is not None:
+                all_plans = all_plans.filter(shopping_date__gte=min_date)
+            max_date = request.GET.get("max_date", None)
+            if max_date is not None:
+                all_plans = all_plans.filter(shopping_date__lte=max_date)
+
             serializer = ExpenseSerializer(all_plans, many=True)
             return JsonResponse(serializer.data, safe=False)
         elif request.method == "POST":
